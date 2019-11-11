b1683cfea8856cda5f5a0d467e659429fb39b725
Animesh Baranawal <animeshbaranawal@gmail.com>
2019-11-11 22:21:37 +0530

    Added code for preprocessing data

---
+++ DataCollection/preProcess.py
@@ -0,0 +1,104 @@
+import os
+import time
+from pathlib import Path
+from bs4 import BeautifulSoup
+
+def preProcessSourceCode(sourceCode):
+    return sourceCode
+
+def getSourceCode(pageSoup):
+    pres = pageSoup.find_all('pre')
+    if pres[0].has_attr("id"):
+        if pres[0]["id"] == "program-source-text":
+            return pres[0].get_text()
+    return ""
+
+def generateSourceCodeStatistics(contest_id, standings, participant_id, submission_id):
+    contest_id_dir = "Contest"+contest_id
+    f = open(contest_id_dir+"/Standings"+standings+"/"+participant_id+"_"+submission_id, 'r')
+    source_code_page = f.read()
+    f.close()
+
+    source_code_soup = BeautifulSoup(source_code_page, 'html.parser')
+    trs = source_code_soup.find_all('tr')
+    tds = trs[1].find_all('td')
+
+    ### extract statistics
+    submissionid = tds[0].get_text().lstrip().split('\n')[0]
+    language = tds[3].get_text().lstrip().split('\n')[0]
+    time = tds[5].get_text().lstrip().split('\n')[0]
+    memory = tds[6].get_text().lstrip().split('\n')[0]
+
+    ### extract source code
+    contest_id_dir = "Contest"+contest_id
+    codes_dir = contest_id_dir+"/Standings"+standings+"/CODES"
+    os.makedirs(codes_dir, exist_ok=True)
+    code_file = Path(codes_dir+"/"+participant_id+"_"+submission_id)
+    if not code_file.is_file():
+        source_code = getSourceCode(source_code_soup)
+
+        if "c++" in language.lower():
+            fw = open(str(code_file)+".cpp",'w')
+        elif "java" in language.lower():
+            fw = open(str(code_file)+".java",'w')
+        elif "pypy" in language.lower():
+            fw = open(str(code_file)+".py",'w')
+        else:
+            fw = open(str(code_file),'w')
+
+        fw.write(source_code)
+        fw.close()
+    else:
+        print("Code already extracted...")
+
+    return (submissionid,language, time, memory)
+
+def generateStandingStatistics(contest_id, problem_ids, topk=5):
+    contest_id_dir = "Contest"+contest_id
+    fw = open(contest_id_dir+"/standings_statistics", 'w') ## write to statistics file
+    fw.write("ParticipantID\tRank\tProblemA_id\tProblemA_memory\tProblemA_time\tProblemA_language\tProblemB_id\tProblemB_memory\tProblemB_time\tProblemB_language\tProblemC_id\tProblemC_memory\tProblemC_time\tProblemC_language\n")
+
+    for i in range(topk):
+        f = open(contest_id_dir+"/standings_"+str(i+1), 'r')
+        standing_page = f.read()
+        f.close()
+
+        page_soup = BeautifulSoup(standing_page, 'html.parser')
+        trs = page_soup.find_all('tr')
+
+        ## the first tr is redundant
+        for participant in range(1,201):
+            line = ""
+            p = trs[participant]
+            participant_id = p['participantid']
+            tds = p.find_all('td')
+
+            line += participant_id
+            line += ('\t' + tds[0].get_text().lstrip().split('\n')[0])
+
+            for pid in problem_ids:
+                found = False
+                for elem in tds: ## get if problem is solved
+                    if elem.has_attr('problemid'):
+                        problem_id = elem['problemid']
+                        if problem_id == pid:
+                            found = True
+                            if elem.has_attr('acceptedsubmissionid'):
+                                accepted_submission_id = elem['acceptedsubmissionid']
+                                submissionid, langauge, time, memory = generateSourceCodeStatistics(contest_id, str(i+1), participant_id, accepted_submission_id)
+                                line += ('\t' + submissionid+"\t"+memory+"\t"+time+"\t"+langauge)
+                            else:
+                                line += ('\t' + "NO\t-\t-\t-")
+
+                if not found:
+                    line += ('\t' + "-\t-\t-\t-")
+            fw.write(line+"\n")
+
+    fw.close()
+
+### preprocess data
+contest_id = "1245"
+problemA_id = "456074"
+problemB_id = "456075"
+problemC_id = "456076"
+generateStandingStatistics(contest_id, [problemA_id, problemB_id, problemC_id])
--- DataCollection/scrapeData.py
+++ DataCollection/scrapeData.py
@@ -1,57 +1,104 @@
 import os
+import time
 from pathlib import Path
 
 import requests
 from bs4 import BeautifulSoup
 
-contest_id = "" #"1245"
-problemA_id = "" #"456074"
-problemB_id = "" #"456075"
-problemC_id = "" #"456076"
+def downloadSourceCodeURL(url):
+    downloadSuccess = False
 
-if contest_id == "":
-    print("Contest ID is blank")
-    exit(1)
+    while not downloadSuccess:
+        try:
+            source_code_page = requests.get(url)
+            assert source_code_page.status_code == 200
+            downloadSuccess = True
+            print("Sleeping...")
+            time.sleep(3) ## wait after every download to give breathing time
+        except requests.exceptions.ConnectionError:
+            print("Connection refused")
+            print("Sleeping for 30 seconds...")
+            time.sleep(30) ## if connection error, wait longer
 
-contest_id_dir = "Contest"+contest_id
-contest_url = "https://codeforces.com/contest/"+contest_id
-os.makedirs(contest_id_dir, exist_ok=True)
+        if downloadSuccess:
+            if "program-source-text" not in source_code_page.text: ## check validity
+                downloadSuccess = False
+            else:
+                break
 
-topk = 5 ### download the top k standing list pages
-for i in range(topk):
-    my_file = Path(contest_id_dir+"/standings_"+str(i+1))
-    if my_file.is_file():
-        print("Standings "+str(i+1)+" already downloaded.")
-        continue
+    return source_code_page
 
-    page = requests.get(contest_url+"/standings/page/"+str(i+1))
-    assert page.status_code == 200
+def downloadContest(contest_id, problem_ids, topk=5):
+    if contest_id == "":
+        print("Contest ID is blank")
+        return
 
-    f = open(contest_id_dir+"/standings_"+str(i+1), 'w')
-    f.write(page.text)
-    f.close()
+    for problem_id in problem_ids:
+        if problem_id == "":
+            print("One of the problem ids is empty")
+            return
 
-for i in range(1):
-    f = open(contest_id_dir+"/standings_"+str(i+1), 'r')
-    page = f.read()
-    f.close()
+    contest_id_dir = "Contest"+contest_id
+    contest_url = "https://codeforces.com/contest/"+contest_id
+    os.makedirs(contest_id_dir, exist_ok=True)
 
-    page_soup = BeautifulSoup(page, 'html.parser')
-    trs = page_soup.find_all('tr')
+    for i in range(topk): ## downloading standing pages
+        my_file = Path(contest_id_dir+"/standings_"+str(i+1))
+        if my_file.is_file():
+            print("Standings "+str(i+1)+" already downloaded.")
+            continue
 
-    ## the first tr is redundant
-    for participant in range(1,201):
-        p = trs[participant]
-        participant_id = p['participantid']
-        print(participant, participant_id)
-        tds = p.find_all('td')
-        for elem in tds:
-            if elem.has_attr('problemid'):
-                problem_id = elem['problemid']
-                if problem_id == problemA_id or problem_id == problemB_id or problem_id == problemC_id:
-                    accepted_submission_id = "-1"
-                    if elem.has_attr('acceptedsubmissionid'):
-                        accepted_submission_id = elem['acceptedsubmissionid']
-                    title = elem['title']
-                    print(problem_id,accepted_submission_id,title)
-        print()
+        page = requests.get(contest_url+"/standings/page/"+str(i+1))
+        assert page.status_code == 200
+
+        f = open(contest_id_dir+"/standings_"+str(i+1), 'w')
+        f.write(page.text)
+        f.close()
+
+    for i in range(topk):
+        f = open(contest_id_dir+"/standings_"+str(i+1), 'r')
+        page = f.read()
+        f.close()
+
+        page_soup = BeautifulSoup(page, 'html.parser')
+        trs = page_soup.find_all('tr')
+
+        standings_dir = contest_id_dir + "/Standings"+str(i+1)
+        os.makedirs(standings_dir, exist_ok=True)
+
+        ## the first tr is redundant
+        for participant in range(1,201):
+            p = trs[participant]
+            participant_id = p['participantid']
+            print(participant, participant_id)
+            tds = p.find_all('td')
+            for elem in tds:
+                if elem.has_attr('problemid'):
+                    problem_id = elem['problemid']
+                    if problem_id in problem_ids:
+                        accepted_submission_id = "-1"
+                        if elem.has_attr('acceptedsubmissionid'):
+                            accepted_submission_id = elem['acceptedsubmissionid']
+                        title = elem['title']
+                        print(problem_id,accepted_submission_id,title)
+
+                        ## download source code
+                        if accepted_submission_id != "-1":
+                            source_code_file = Path(standings_dir+"/"+participant_id+"_"+accepted_submission_id)
+                            if source_code_file.is_file():
+                                print("Source code for submissionID "+accepted_submission_id+" for participant "+participant_id+" already downloaded.")
+                                continue
+
+                            source_code_page = downloadSourceCodeURL(contest_url+"/submission/"+accepted_submission_id)
+
+                            f = open(standings_dir+"/"+participant_id+"_"+accepted_submission_id, 'w')
+                            f.write(source_code_page.text)
+                            f.close()
+            print()
+
+### download Data
+contest_id = "1245"
+problemA_id = "456074"
+problemB_id = "456075"
+problemC_id = "456076"
+downloadContest(contest_id, [problemA_id, problemB_id, problemC_id])
